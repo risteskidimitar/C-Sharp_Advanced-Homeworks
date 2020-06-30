2898a8939c8e5b6a13df71a455e3be25aa255de3
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-06-30 10:06:16 +0200

    c

---
+++ TablicaMnozenje/TablicaMnozenje/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TablicaMnozenje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyFile = @"Result.txt";
+
+            List<string> List = new List<string>();
+            string Result;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Result = $"{i} * {j} = {i * j}";
+                    List.Add(Result);
+                    File.WriteAllLines(MyFile, List);
+                    Console.WriteLine(Result);
+                }
+                string line = $"_______________";
+                List.Add(line);
+                File.WriteAllLines(MyFile, List);
+                Console.WriteLine(line);
+                
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
