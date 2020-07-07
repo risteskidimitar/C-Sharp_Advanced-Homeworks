ea0b49a8646b67302143c9a8ac35bcb2ae51c936
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-07-07 14:11:29 +0200

    aa

---
+++ AsyncAwait/FileSystemExercise/FileSystemExercise/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileSystemExercise
+{
+    class Program
+    {
+        public static string Writer(int num1, int num2)
+        {
+            var result = $"{num1} + {num2} = {num1 + num2}";
+            Console.WriteLine(result);
+            return result;
+        }
+        public static async Task TaskAsyncAwait(string path, bool boolTrue)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Hello, please enter two numbers");
+                Console.WriteLine("Your first number is... :");
+
+                if (!int.TryParse(Console.ReadLine(), out int num1))
+                {
+                    continue;
+                }
+                Console.WriteLine("Your second number is... :");
+                if (!int.TryParse(Console.ReadLine(), out int num2))
+                {
+                    continue;
+                }
+
+                using (StreamWriter sw = new StreamWriter(path, boolTrue))
+                {
+                    await sw.WriteLineAsync(Writer(num1, num2));
+                }
+
+                Console.WriteLine("For additional calculation press Y-y or any key for exit");
+                string additional = Console.ReadLine();
+                if (additional == "Y" || additional == "y")
+                {
+                    continue;
+                }
+                break;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string directoryPath = Path.Combine(currentDirectory, "Exercise");
+            string filePath = Path.Combine(directoryPath, "Calculations.txt");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close(); //Ova ne mora StreamWriter-ot sam kreira fajl dokolku ne postoi.             
+            }
+
+            TaskAsyncAwait(filePath, true).Wait();
+
+        }
+    }
+}
---
+++ AsyncAwait/TablicaMnozenje/TablicaMnozenje/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TablicaMnozenje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyFile = @"Result.txt";
+
+            List<string> List = new List<string>();
+            string Result;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Result = $"{i} * {j} = {i * j}";
+                    List.Add(Result);
+                    //Console.WriteLine(Result);
+                }
+                string line = $"_______________";
+                List.Add(line);
+               // Console.WriteLine(line);
+            }
+
+            AsyncWriter(MyFile, List);
+
+            Console.WriteLine("I'm still working!!!");
+
+            Console.ReadLine();
+        }
+        public static async Task AsyncWriter(string file, List<string> list)
+        {
+            await File.WriteAllLinesAsync(file, list);
+            await Task.Run(() => 
+            {
+                Thread.Sleep(3000);              
+                list.ForEach(l => Console.WriteLine(l));
+            });
+         
+
+        }
+    }
+}
